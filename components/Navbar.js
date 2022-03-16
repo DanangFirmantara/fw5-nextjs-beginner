4b49c26c1feb05b58f6a779f8ee3337cdfb82722
DanangFirmantara <danangfirmantara837@gmail.com>
2022-03-16 16:41:24 +0700

    feat : initial

---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+const Navbar = () =>{
+   return (
+      <div className='d-flex justify-content-center'>Navbar</div>
+   )
+}
+
+export default Navbar
---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+
+const Login = () =>{
+   return (
+      <>
+         <Navbar />
+         <div>login</div>
+      </>
+   )
+}
+export default Login
