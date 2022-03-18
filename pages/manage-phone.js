f2c9371dded1b1381c508c478b3e8871bfbe3171
DanangFirmantara <danangfirmantara837@gmail.com>
2022-03-19 01:41:26 +0700

    feat : add manage phone number

--- pages/manage-phone.js
+++ pages/manage-phone.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import { Row, Col, Container } from 'react-bootstrap'
+import FooterUser from '../components/FooterUser'
+import NavbarUser from '../components/NavbarUser'
+import SidebarUser from '../components/SidebarUser'
+import { useRouter } from 'next/router'
+import {RiDeleteBin7Line} from 'react-icons/ri'
 
 const ManagePhoneNumber = () => {
+	const router = useRouter()
+	const onDelete = ()=>{
+		router.push('/manage-phone')
+	}
 	return (
-		<div>ManagePhoneNumber</div>
+		<>
+			<NavbarUser/>
+			<Container className='vh-100'>
+				<Row className='mt-5'>
+					<Col sm={4}>
+						<SidebarUser />
+					</Col>
+					<Col>
+						<div className='bg-white shadow-dark rounded-2 sidebar-user'>
+							<Container className='py-5'>
+								<div className='d-flex justify-content-between align-items-center'>
+									<div className='fw-bold fs-6'>Personal Information</div>
+								</div>
+								<div className='d-flex justify-content-between align-items-center my-4'>
+									<div className='w-50 text-pallet-6'>
+									You can only delete the phone number and then you must add another phone number.
+									</div>
+								</div>
+								<div className='w-100 py-3 px-3 d-flex align-items-center position-relative'>
+									<div>
+										<div className='fs-7 text-muted'>Primary</div>
+										<div className='fw-bold fs-4'>+62 813-9387-7946</div>
+									</div>
+									<button className='bg-transparent border-0 position-absolute fs-7 fw-semibold text-pallet-6 end-0 me-5' onClick={onDelete}><RiDeleteBin7Line className='fs-4 deleted'/></button>
+								</div>
+							</Container>
+							
+						</div>
+					</Col>
+				</Row>
+			</Container>
+			<FooterUser />
+		</>
 	)
 }
 
