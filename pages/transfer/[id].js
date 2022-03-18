26de87a66ef6bd43aab9becbedd65ef457a0efa2
DanangFirmantara <danangfirmantara837@gmail.com>
2022-03-18 19:59:48 +0700

    feat : add confirmation

--- pages/transfer/[id].js
+++ pages/transfer/[id].js
@@ -5,8 +5,14 @@ import NavbarUser from '../../components/NavbarUser'
 import SidebarUser from '../../components/SidebarUser'
 import Image from 'next/image'
 import {BsPencil} from 'react-icons/bs'
+import {useRouter} from 'next/router'
 
 const ReceiverUser = () => {
+	const router = useRouter()
+	const onTransfer = (event)=>{
+		event.preventDefault()
+		router.push('/confirmation')
+	}
 	return (
 		<>
 			<NavbarUser/>
@@ -35,7 +41,7 @@ const ReceiverUser = () => {
 									<div className='text-pallet-6 fs-7 '>press continue to the next steps.</div>
 								</div>
 								<div className='d-flex align-items-center justify-content-center'>
-									<form className='d-flex flex-column justify-content-center align-items-center'>
+									<form className='d-flex flex-column justify-content-center align-items-center' onSubmit={onTransfer}>
 										<input placeholder='0.00' className='text-center mt-5 mb-3 border-0 fs-1'/>
 										<div className='fw-bold fs-7 mb-5'>Rp.120.000 Available</div>
 										<div className='d-flex position-relative align-items-center mb-4'>
@@ -43,7 +49,7 @@ const ReceiverUser = () => {
 											<BsPencil className='position-absolute left-0 fs-4 text-pallet-6 ms-1'/>
 										</div>
 										<div className='my-5 w-100'>
-											<button className='btn-pallet-1 text-white border-0 px-4 py-3 fs-5 rounded-2 shadow-dark w-100'>Continue</button>
+											<button className='btn-pallet-1 text-white border-0 px-4 py-3 fs-5 rounded-2 shadow-dark w-100' type='submit'>Continue</button>
 										</div>
 									</form>
 								</div>
